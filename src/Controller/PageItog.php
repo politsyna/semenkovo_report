5ee13c608c2264111505d29e9f749b97639bf5f8
www-data <www-data@02b3d5e9eef6>
2017-03-14 23:04:58 +0300

    add new info

--- src/Controller/PageItog.php
+++ src/Controller/PageItog.php
@@ -17,6 +17,10 @@ class PageItog extends ControllerBase {
   public function report($start, $end) {
     $orders = $this->getOrders();
     $source = [];
+    $all_programm = [];
+    $vsego_programm = 0;
+    $all_people = [];
+    $vsego_people = 0;
     foreach ($orders as $key => $node) {
       $fieldcollection = $node->field_orders_visitor;
       $fc = Group::collectionItems($fieldcollection);
@@ -33,16 +37,43 @@ class PageItog extends ControllerBase {
           'О работника' => $middlename,
         ];
       }
-      //dsm($team);
-      $source[] = [
+      $node_usluga = $node->field_orders_ref_activity->entity;
+      $termin = $node_usluga->field_activity_type->entity;
+      $name_programm = $termin->name->value;
+      $source = [
         'название заявки' => $node->title->value,
         'работники' => $team,
         'категории, регионы и кол-во посетителей' => $fc,
         'общее количество людей' => $node->field_orders_group->value,
         'фактическая стоимость услуги' => $node->field_orders_cost->value,
+        'тип программы' => $node->field_orders_ref_activity->entity->field_activity_type->entity->name->value,
       ];
+      // Считаем количество проведенных программ.
+      $kolvo_programm = 1;
+      if (!isset($all_programm[$name_programm])) {
+        $all_programm[$name_programm] = [
+          'name_programm' => $name_programm,
+          'summa' => 0,
+        ];
+      }
+      // 1. Работаем с участком массива $all_programm под названием $name_programm, например, Солнцеворот.
+      // 2. Внутри этого участка берем значение из поля 'summa'.
+      $current_summa = $all_programm[$name_programm]['summa'];
+      // 3. Добавляем к нему 1 ($kolvo_programm).
+      $new_summa = $current_summa + 1;
+      // 4. Полученное значение кладем в 'summa'.
+      $all_programm[$name_programm]['summa'] = $new_summa;
+      // 5. Считаем сколько всего программ проведено (= сколько циклов прошло).
+      $vsego_programm = $vsego_programm + $kolvo_programm;
+      
+      // Считаем посещаемость.
+      $people = $source['общее количество людей'];
+      $id = $node->id();
+      $all_people[$id] = [
+        'количество людей' => $source['общее количество людей'],
+      ];
+      $vsego_people = $vsego_people + $all_people[$id]['количество людей'];
     }
-    //dsm($source);
 
     $teams = $this->getTeam();
     $team = [];
@@ -53,7 +84,6 @@ class PageItog extends ControllerBase {
       ];
     }
     $exhour = $this->getExhour();
-    // chas = [];
     $vsego_chasov = 0;
     foreach ($exhour as $key => $node_exhour) {
       $tid = $node_exhour->field_exhour_team->entity->id();
@@ -77,7 +107,11 @@ class PageItog extends ControllerBase {
     $data = [
       'reportmonth' => format_date(strtotime($end), 'custom', 'M Y'),
       'team' => $team,
-      'vsego' => $vsego_chasov,
+      'vsego_chasov' => $vsego_chasov,
+      'all_programm' => $all_programm,
+      'vsego_programm' => $vsego_programm,
+      'all_people' => $all_people,
+      'vsego_people' => $vsego_people,
     ];
 
     $renderable['h'] = array(
@@ -89,7 +123,7 @@ class PageItog extends ControllerBase {
   }
 
   /**
-   * A getOgders.
+   * Функциями getХххх() формируем из ноды объекты.
    */
   public function getOrders() {
     $query = \Drupal::entityQuery('node');
